4f975424e466b80652ddd3330a3e415278343be7
Gerard <gerardalvariz@gmail.com>
2022-06-17 20:31:26 +0200

    Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+
+
+
 const App = () => {
   return (
     <div>
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Article } from './Article';
+export { default as Nabvar } from './Nabvar';
+export { default as Footer } from './Footer';
