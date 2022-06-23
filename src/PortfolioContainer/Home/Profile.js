7898100c56a088f157791e7aaab5cdd76ab6be0e
Gerard <77858157+gerardalvarez@users.noreply.github.com>
2022-06-23 19:15:31 +0200

    Logo y empezando navbar responsive

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -46,7 +46,6 @@ export default function Profile() {
           </div>
         </div>
       </div>
-      <div class='light x1'></div><div class='light x2'></div><div class='light x3'></div><div class='light x4'></div><div class='light x5'></div><div class='light x6'></div><div class='light x7'></div><div class='light x8'></div><div class='light x9'></div>
     </div></>
   );
 }
