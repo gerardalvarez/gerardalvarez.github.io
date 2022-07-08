4777037027d33ccad72e33b4c1c05270b1ba9998
Gerard <gerardalvariz@gmail.com>
2022-07-08 20:40:21 +0200

    particulas

--- src/components/Footer/Contact.js
+++ src/components/Footer/Contact.js
@@ -46,49 +46,54 @@ const Contact = () => {
   }, 8000);
   return (
     <div className="fondo-con">
-      <div class="wrapper2" id="Contact">
-        <div class="inner2">
+      <div className="wrapper2" id="Contact">
+        <div className="inner2">
           <form ref={form} onSubmit={sendEmail}>
             <h3 className="con">Contact Me</h3>
             <p className="c">
               Call my phone +34 608315018 or leave a message down below
             </p>
-            <label class="form-group">
+            <label className="form-group">
               <input
                 type="text"
                 name="user_name"
-                class="form-control"
+                className="form-control"
                 required
               />
               <span>Your Name</span>
-              <span class="border"></span>
+              <span className="border"></span>
             </label>
-            <label class="form-group">
+            <label className="form-group">
               <input
                 type="email"
                 name="user_email"
-                class="form-control"
+                className="form-control"
                 required
               />
-              <span for="">Your Mail</span>
-              <span class="border"></span>
+              <span>Your Mail</span>
+              <span className="border"></span>
             </label>
-            <label class="form-group">
-              <textarea name="message" id="" class="form-control" required />
-              <span for="">Your Message</span>
-              <span class="border"></span>
+            <label className="form-group">
+              <textarea
+                name="message"
+                id=""
+                className="form-control"
+                required
+              />
+              <span>Your Message</span>
+              <span className="border"></span>
             </label>
             <button>
               Submit
-              <i class="zmdi zmdi-arrow-right"></i>
+              <i className="zmdi zmdi-arrow-right"></i>
             </button>
           </form>
           {result && <Result />}
         </div>
 
-        <div class="foot">Made by Gerard Álvarez</div>
-        <div class="footer-mov">
-          <div class="iconos-redes">
+        <div className="foot">Made by Gerard Álvarez</div>
+        <div className="footer-mov">
+          <div className="iconos-redes">
             <motion.div
               className="su2"
               initial={{ opacity: 0, x: -20 }}
@@ -142,8 +147,8 @@ const Contact = () => {
               />
             </motion.div>
           </div>
-          <div class="nombre">gerardalvariz@gmail.com</div>
-          <div class="nombre2">Made by Gerard Álvarez</div>
+          <div className="nombre">gerardalvariz@gmail.com</div>
+          <div className="nombre2">Made by Gerard Álvarez</div>
         </div>
       </div>
     </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,101 @@
 import React from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useEffect, useState } from "react";
-import XD from "./twoniggas.jpg";
 import "./projects.css";
-import { ScaleLoader } from "react-spinners";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 const Projects = () => {
-  return <div className="container"></div>;
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  return (
+    <div className="container" style={{ position: "relative" }}>
+      <div
+        className="particles-container"
+        style={{ position: "absolute", width: "100%" }}
+      >
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            fpsLimit: 30,
+            interactivity: {
+              events: {
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 1,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 3,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 40,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
+            },
+            detectRetina: false,
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
